87a2fb9c8d78607de4587fb25e51e5c419622506
VolodymyrBodniak <bodnjak@gmail.com>
2023-07-06 22:01:48 +0400

    Update hw-07 task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryElement = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img
+          class="gallery__image"
+          src="${item.preview}"
+          data-source="${item.original}"
+          alt="${item.description}"
+        />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryElement.innerHTML = addGalleryMarkup;
+galleryElement.addEventListener("click", onImageClick);
+
+function onImageClick(evt) {
+  blockStandardAction(evt);
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}" width="800" height="600">`
+  );
+  instance.show();
+
+  galleryElement.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
+function blockStandardAction(evt) {
+  evt.preventDefault();
+}
